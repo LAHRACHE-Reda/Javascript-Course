7812cd71989aaee3ab58846b0ccf7926940d01e2
Reda <reda.lahrache@outlook.com>
2021-06-22 04:11:09 +0100

    Javascript : objects

--- app/js/index.js
+++ app/js/index.js
@@ -4,5 +4,27 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('First Course')
+logTitle('Objects')
 /* coding examples */
+
+// objet
+var person = {
+    name: "Lahrache Reda",
+    age: 23,
+    heigh : 184,
+    address: {   // objet comme attribut d'un autre objet
+        pays: "Maroc",
+        ville: "Casablanca",
+        rue: "Hay artisanat"
+    }
+};
+
+// afficher l'objet sous forme string json
+log("**l'objet person sous forme json: "+JSON.stringify(person));
+
+// accéder aux attributs de person
+log("**la valeur de l'attribut nom: "+person.name);
+
+// afficher les valeurs de l'objet
+log("**Les valeurs de l'objet person: "+Object.values(person));
+log("**Les valeurs de l'objet adresse: "+Object.values(person.address));
