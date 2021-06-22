645e10e4cd7efa7258180fdef1348addcb8a88c1
Reda <reda.lahrache@outlook.com>
2021-06-22 15:26:21 +0100

    Javascript : Variables

--- app/js/index.js
+++ app/js/index.js
@@ -4,27 +4,16 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Objects')
+logTitle('Variables')
 /* coding examples */
 
-// objet
-var person = {
-    name: "Lahrache Reda",
-    age: 23,
-    heigh : 184,
-    address: {   // objet comme attribut d'un autre objet
-        pays: "Maroc",
-        ville: "Casablanca",
-        rue: "Hay artisanat"
-    }
-};
 
-// afficher l'objet sous forme string json
-log("**l'objet person sous forme json: "+JSON.stringify(person));
+var name = "Reda";             // string
+var age = 23;                  // number
+var hasDriverLicence = true;   // Boolean
+var empty = undefined;         // variable qui ne contient rien
 
-// accéder aux attributs de person
-log("**la valeur de l'attribut nom: "+person.name);
-
-// afficher les valeurs de l'objet
-log("**Les valeurs de l'objet person: "+Object.values(person));
-log("**Les valeurs de l'objet adresse: "+Object.values(person.address));
+log(name+" --> Type : "+typeof name);
+log(age+" --> Type : "+typeof age);
+log(hasDriverLicence+" --> Type : "+typeof hasDriverLicence);
+log(empty+" --> Type : "+typeof empty);
